d8522bcd5da1bedb3d7e5d472e1c7ac6e336f325
William Rodrigues <rzdevops@hotmail.com>
2020-05-06 16:59:45 -0300

    Primeira versão, projeto para ver salário e determinada renda de funcíonário

---
+++ Proj1/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proj1.Entities
+{
+	class Department
+	{
+		public string Name { get; set; }
+
+		public Department()
+		{
+
+		}
+
+		public Department(string name)
+		{
+
+			Name = name;
+
+		}
+		
+	}
+}
